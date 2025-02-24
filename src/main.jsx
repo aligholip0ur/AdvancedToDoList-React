8c7821aea4b1a4ee3e4ffa2e1a25cf721f172744
Ali Gholipour <Ali1383gho@gmail.com>
2025-02-24 17:34:30 +0330

    resolive error

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Home from './Components/Home'
+import Home from './components/Home'
 import '../src/css/css.css'
 
 
